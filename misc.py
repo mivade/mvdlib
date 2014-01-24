afd8274fcc5fa3073eab4302c5283c004d4191ed
Michael V. DePalatis <depalatis@phys.au.dk>
2014-01-24 10:18:55 +0100

    RMS function added.

--- misc.py
+++ misc.py
@@ -19,3 +19,8 @@ def fix_offset_and_normalize(data):
     """Removes dc offsets and normalizes data."""
     offset = remove_offset(data)
     return normalize(offset)
+
+def rms(x):
+    """Return the root mean square value of the array x."""
+    return np.sqrt(np.sum(x**2)/len(x))
+
