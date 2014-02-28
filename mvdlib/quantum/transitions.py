99c9759002ac50cb9cd063632453807eee13e4ed
Michael V. DePalatis <depalatis@phys.au.dk>
2014-02-28 17:37:44 +0100

    Added more conversions.

--- mvdlib/quantum/transitions.py
+++ mvdlib/quantum/transitions.py
@@ -16,10 +16,31 @@ from __future__ import division
 import numpy as np
 from scipy.constants import h, c, physical_constants as consts
 
+# Units
+# -----
+
 # Conversion factors from non-SI to SI
 _wl_units = {'nm': 1e-9, 'um': 1e-6, 'angstrom': 1e-10}
 _f_units = {'Hz': 1.0, 'kHz': 1e3, 'MHz': 1e6, 'THz': 1e12}
 _E_units = {'eV': consts['electron volt'][0], 'J': 1.0}
+
+# Error messages
+# --------------
+
+def _units_err_msg(measure):
+    if measure == 'wl':
+        units = _wl_units
+        name = "Wavelength"
+    elif measure == 'f':
+        units = _f_units
+        name = "Frequency"
+    elif measure == 'E':
+        units = _E_units
+        name = "Energy"
+    print(name, "must be one of:", units.keys())
+
+# Functions
+# ---------
 
 def wavenumber_to_frequency(wavenumber, angular=False):
     """
@@ -142,6 +163,67 @@ def frequency_to_wavelength(freq, wl_units="nm", angular=False):
     else:
         raise ValueError("wl_units must be nm, um, or angstroms.")
 
+def frequency_to_energy(freq, f_units="THz", E_units="eV"):
+    """
+    Convert a frequency to an energy.
+
+    Parameters
+    ----------
+    freq : float
+        Frequency.
+    f_units : str, optional
+        Units the frequency is given in.
+    E_units : str, optional
+        Units for the returned energy value.
+
+    Returns
+    -------
+    E : float
+        The energy corresponding to the given wavelength.
+
+    """
+    try:
+        f_SI = freq*_f_units[f_units]
+    except KeyError:
+        _units_err_msg('f')
+    E_SI = h*f_SI
+    try:
+        E = E_SI/E_units
+    except KeyError:
+        _units_err_msg('E')
+    return E
+
+def wavelength_to_frequency(wl, wl_units="nm", f_units="THz"):
+    """
+    Convert a wavelength to frequency.
+
+    Parameters
+    ----------
+    wl : float
+        Wavelength.
+    wl_units : str, optional
+        Units the wavelength is given in.
+    f_units : str, optional
+        Units for the returned frequency value.
+
+    Returns
+    -------
+    f : float
+        The frequency corresponding to the given wavelength.
+
+    """
+    try:
+        wl_SI = wl*_wl_units[wl_units]
+    except KeyError:
+        _units_err_msg('wl')
+    f_SI = c/wl_SI
+    try:
+        f = f_SI/_f_units[f_units]
+    except KeyError:
+        _units_err_msg('f')
+    return f
+
+
 def wavelength_to_energy(wl, wl_units="nm", E_units="eV"):
     """
     Convert a wavelength to energy.
@@ -164,13 +246,42 @@ def wavelength_to_energy(wl, wl_units="nm", E_units="eV"):
     try:
         E_SI = h*c/(wl*_wl_units[wl_units])
     except KeyError:
-        print("Wavelength units must be one of: ", _wl_units.keys())
+        _units_err_msg('wl')
     try:
         E = E_SI/_E_units[E_units]
     except KeyError:
-        print("Energy units must be one of: ", _E_units.keys())
+        _units_err_msg('E')
     return E
-        
+
+def energy_to_frequency(E, E_units="eV", f_units="THz"):
+    """
+    Convert energy to frequency.
+
+    Parameters
+    ----------
+    E : float
+        Energy.
+    E_units : str, optional
+        Units the energy is given in.
+    f_units : str, optional
+        Units for the returned frequency value.
+
+    Returns
+    -------
+    f : float
+        The frequency corresponding to the given energy.
+
+    """
+    try:
+        E_SI = E*_E_units[E_units]
+    except KeyError:
+        _units_err_msg('E')
+    f_SI = E_SI/h
+    try:
+        f = f_SI/_f_units[f_units]
+    except KerError:
+        _units_err_msg('f')
+    return f
 
 if __name__ == "__main__":
     wavenum = 25191.51 # wavenumber in 1/cm for Ca+ 397 nm transition
